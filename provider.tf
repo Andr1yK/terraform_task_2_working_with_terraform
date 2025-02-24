56565f7053a1b79390fd1f2c0f11e58e8d417bba
Andriy Kobetiak <andriy.k@mate.academy>
2025-02-25 00:51:49 +0200

    change-comment-place

--- provider.tf
+++ provider.tf
@@ -1,6 +1,3 @@
-// I got trouble with Azure provider :(
-// I was not able to solve the issue with registering Azure provider, so I decided to use AWS provider
-
 provider "aws" {
   region = var.region
 }
